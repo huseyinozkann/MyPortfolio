9f72fd4c64cafe05e8708def2c547d5ae5d2f991
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-22 16:01:23 +0300

    Feature Entity added

---
+++ MyPortfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,9 @@
+namespace MyPortfolio.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}

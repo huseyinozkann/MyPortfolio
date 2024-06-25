2d170a197e7a1a3487e1725b013e24caa8a612e7
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-26 01:07:49 +0300

    Portfolio Component View Partial added

--- MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8465dfe343cbe0a6f18dd4226e70e51a126cf98b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

6386ba3b88347b184689ed7b81dfd62ce4d5cb9c
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-27 16:02:47 +0300

    Footer Component View Partial added

---
+++ MyPortfolio/ViewComponents/_FooterComponentPartial.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace  MyPortfolio.ViewComponents
+{
+    public class _FooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
